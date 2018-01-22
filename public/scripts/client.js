29cdfcafe4f412c2a17dcd81fba6858eb7dd3a51
Sean Felling <Snfelling7@gmail.com>
2018-01-22 14:38:01 -0600

    added intervals to carousel

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -2,6 +2,12 @@ $(document).ready( function() {
 
 $('.carousel.carousel-slider').carousel({fullWidth: true});
 
+$('.carousel').carousel();
+ setInterval(function() {
+   $('.carousel').carousel('next');
+ }, 4500);
+
+
 
   $(".button-collapse").sideNav();
 
